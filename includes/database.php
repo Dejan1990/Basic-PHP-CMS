24af9fa23253247d0dbd89ca5eb79d7e3ab108ff
Dejan Simeunovic <dejan@pop-os.localdomain>
2023-05-09 16:26:26 +0200

    Initial commit

---
+++ includes/database.php
@@ -0,0 +1,5 @@
+<?php
+
+$connect = mysqli_connect('database', 'root', 'tiger', 'php_cms_db');
+
+//if ($connect) echo 'connected';
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP Cms</title>
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+    <h1>Basic CMS</h1>
+
+    <div>
+        <a href="logout.php">Logout</a>
+        <a href="dashboard.php">Dashboard</a>
+    </div>
+
+    <hr>
+
+    <div>
+    
